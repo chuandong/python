d02e1ef91d8ba552f01ce636077996c157ee87ea
chuandong mark <chuandong mark>
2016-11-01 23:00:09 +0800

    code

---
+++ class_set.py
@@ -0,0 +1,13 @@
+class Duck():
+    def __init__(self,input_name):
+        self.hidden_name=input_name
+
+    @property
+    def name(self):
+        print('inside the getter')
+        return self.hidden_name
+    @name.setter
+    def name(self,input_name):
+        print('insid the setter')
+        self.hidden_name=input_name
+
